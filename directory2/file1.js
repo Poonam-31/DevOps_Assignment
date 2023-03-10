97ff99763ccdc271758e405c6b717d352c2b62a8
Poonam Chandra <poonamchandra@197NODMB3079143.bbrouter>
2023-03-10 21:09:24 +0530

    Updated the file with new changes.

--- directory2/file1.js
+++ directory2/file1.js
@@ -1,2 +1,2 @@
 console.log("This is file one in directory1.");
-console.log('In this exercise we will see how to pdate a source code file and display the difference');
+console.log('In this exercise we will see how to update source code file and display the difference')
